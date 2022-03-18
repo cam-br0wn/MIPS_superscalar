eb0d27446db3f1068b7a6217501c256c9e6418f7
Cam Brown <cambrown@u.northwestern.edu>
2022-03-17 22:59:16 -0500

    finalized inputs and outputs

--- ce361_CPU_Verilog/cpu/SS_HazDet.v
+++ ce361_CPU_Verilog/cpu/SS_HazDet.v
@@ -7,9 +7,12 @@ module SS_HazDet (
     pipeA_dest, pipeA_src1, pipeA_src2,
     pipeB_dest, pipeB_src1, pipeB_src2,
     pipeC_dest, pipeC_src1,
-    inst1_inst_out, inst1_pc_out,
-    inst2_inst_out, inst2_pc_out,
-    inst3_inst_out, inst3_pc_out
+    inst1_pc_out,
+    inst2_pc_out,
+    inst3_pc_out,
+    pipeA_op_in, pipeA_dest_in, pipeA_src1_in, pipeA_src2_in, pipeA_func_in, pipeA_shamt_in, pipeA_imm_in,
+    pipeB_op_in, pipeB_dest_in, pipeB_src1_in, pipeB_src2_in, pipeB_func_in, pipeB_shamt_in, pipeB_imm_in,
+    pipeC_op_in, pipeC_dest_in, pipeC_src1_in, pipeC_imm_in,
     );
 
     // alpha, beta && gamma are labels from instructions being fed into hazard detection
@@ -50,9 +53,34 @@ module SS_HazDet (
     reg [31:0] third_inst;
 
     // instruction output of each instruction after ordering
-    output reg [31:0] inst1_inst_out;
-    output reg [31:0] inst2_inst_out;
-    output reg [31:0] inst3_inst_out;
+    reg [31:0] inst1_inst_out;
+    reg [31:0] inst2_inst_out;
+    reg [31:0] inst3_inst_out;
+
+    // corresponding pipeline wires
+    // Pipeline A -- ALU 1
+    output wire [5:0] pipeA_op_in;
+    output wire [4:0] pipeA_dest_in;
+    output wire [4:0] pipeA_src1_in;
+    output wire [4:0] pipeA_src2_in;
+    output wire [5:0] pipeA_func_in;
+    output wire [4:0] pipeA_shamt_in;
+    output wire [15:0] pipeA_imm_in;
+
+    // Pipeline B -- ALU 2
+    output wire [6:0] pipeB_op_in;
+    output wire [4:0] pipeB_dest_in;
+    output wire [4:0] pipeB_src1_in;
+    output wire [4:0] pipeB_src2_in;
+    output wire [5:0] pipeB_func_in;
+    output wire [4:0] pipeB_shamt_in;
+    output wire [15:0] pipeB_imm_in;
+
+    // Pipeline C -- Memory Ops
+    output wire [5:0] pipeC_op_in;
+    output wire [4:0] pipeC_dest_in;
+    output wire [4:0] pipeC_src1_in;
+    output wire [15:0] pipeC_imm_in;
 
     // program counter output of each instruction after ordering (to be passed to pipelines/instruction fetch)
     output reg [31:0] inst1_pc_out;
@@ -834,9 +862,200 @@ module SS_HazDet (
                 
             end
         end
+        // Hallelujah, we have established who is stalling and all that crap
+
+        // flag for if pipelines have been occupied by an instruction this cycle
+        integer A_busy;
+        integer B_busy;
+        integer C_busy;
+        A_busy = 0;
+        B_busy = 0;
+        C_busy = 0;
+
+        // Now onward to sending instructions to pipelines as necessary
+        if(!inst1_stall) begin
+            
+            // if instruction 1 is a memory op
+            if ((inst1_op == 6'b101011) || (inst1_op == 6'b100011)) begin
+                // send inst1 to pipeline C
+                pipeC_op_in = inst1_op;
+                pipeC_dest_in = inst1_dest;
+                pipeC_src1_in = inst1_src1;
+                pipeC_imm_in = inst1_immediate;
+
+                C_busy = 1;
+            end
+            // otherwise send it to an ALU pipeline
+            else begin
+
+                // default sending to A because this is the first instruction
+                pipeA_op_in = inst1_op;
+                pipeA_dest_in = inst1_dest;
+                pipeA_src1_in = inst1_src1;
+                pipeA_src2_in = inst1_src2;
+                pipeA_func_in = inst1_func;
+                pipeA_shamt_in = inst1_shift_amt;
+                pipeA_imm_in = inst1_immediate;
+
+                A_busy = 1;
+                
+            end
+
+            // instruction 2
+            if(!inst2_stall) begin
+                
+                // if instruction 2 is a memory op and instruction 1 did not already occupy pipe C this cycle
+                if (((inst2_op == 6'b101011) || (inst2_op == 6'b100011)) && (!C_busy)) begin
+                    // send inst1 to pipeline C
+                    pipeC_op_in = inst2_op;
+                    pipeC_dest_in = inst2_dest;
+                    pipeC_src1_in = inst2_src1;
+                    pipeC_imm_in = inst2_immediate;
+
+                    C_busy = 1;
+                end
+
+                // if instruction 2 is a ALU operation and either pipe A or pipe B are available
+                else if (!A_busy && !((inst2_op == 6'b101011) || (inst2_op == 6'b100011))) begin
+                    pipeA_op_in = inst2_op;
+                    pipeA_dest_in = inst2_dest;
+                    pipeA_src1_in = inst2_src1;
+                    pipeA_src2_in = inst2_src2;
+                    pipeA_func_in = inst2_func;
+                    pipeA_shamt_in = inst2_shift_amt;
+                    pipeA_imm_in = inst2_immediate;
+
+                    A_busy = 1;
+                end
+                
+                // if instruction 2 is an ALU op and only pipeline B is available
+                else if (!((inst2_op == 6'b101011) || (inst2_op == 6'b100011))) begin
+                    pipeB_op_in = inst2_op;
+                    pipeB_dest_in = inst2_dest;
+                    pipeB_src1_in = inst2_src1;
+                    pipeB_src2_in = inst2_src2;
+                    pipeB_func_in = inst2_func;
+                    pipeB_shamt_in = inst2_shift_amt;
+                    pipeB_imm_in = inst2_immediate;
+
+                    B_busy = 1;
+                end
+
+                else begin
+                    
+                    // send signals to stall instruction 2 and 3
+                    // this one is necessary, because it could be that inst's 1 and 2 are both memory ops, in which case, not enough piplines for both
+                    inst2_pc_out = second_pc;
+                    inst3_pc_out = third_pc;
+
+                    // Pipeline A -- ALU 1
+                    pipeA_op_in = 6'b000000;
+                    pipeA_dest_in = 5'b00000;
+                    pipeA_src1_in = 5'b00000;
+                    pipeA_src2_in = 5'b00000;
+                    pipeA_func_in = 6'b000000;
+                    pipeA_shamt_in = 5'b00000;
+                    pipeA_imm_in = 16'h0000;
+
+                    // Pipeline B -- ALU 2
+                    pipeB_op_in = 6'b000000;
+                    pipeB_dest_in = 5'b00000;
+                    pipeB_src1_in = 5'b00000;
+                    pipeB_src2_in = 5'b00000;
+                    pipeB_func_in = 6'b000000;
+                    pipeB_shamt_in = 5'b00000;
+                    pipeB_imm_in = 6'h0000;
 
 
+                end
 
+                if (!inst3_stall) begin
+                    
+                    // if instruction 2 is a memory op and instruction 1 did not already occupy pipe C this cycle
+                    if (((inst3_op == 6'b101011) || (inst3_op == 6'b100011)) && (!C_busy)) begin
+                        // send inst1 to pipeline C
+                        pipeC_op_in = inst3_op;
+                        pipeC_dest_in = inst3_dest;
+                        pipeC_src1_in = inst3_src1;
+                        pipeC_imm_in = inst3_immediate;
+
+                        C_busy = 1;
+                    end
+
+                    // if instruction 2 is a ALU operation and either pipe A or pipe B are available
+                    else if (!A_busy && !((inst3_op == 6'b101011) || (inst3_op == 6'b100011))) begin
+                        pipeA_op_in = inst3_op;
+                        pipeA_dest_in = inst3_dest;
+                        pipeA_src1_in = inst3_src1;
+                        pipeA_src2_in = inst3_src2;
+                        pipeA_func_in = inst3_func;
+                        pipeA_shamt_in = inst3_shift_amt;
+                        pipeA_imm_in = inst3_immediate;
+
+                        A_busy = 1;
+                    end
+                    
+                    // if instruction 2 is an ALU op and only pipeline B is available
+                    else if (!B_busy && !((inst3_op == 6'b101011) || (inst3_op == 6'b100011))) begin
+                        pipeB_op_in = inst3_op;
+                        pipeB_dest_in = inst3_dest;
+                        pipeB_src1_in = inst3_src1;
+                        pipeB_src2_in = inst3_src2;
+                        pipeB_func_in = inst3_func;
+                        pipeB_shamt_in = inst3_shift_amt;
+                        pipeB_imm_in = inst3_immediate;
+
+                        B_busy = 1;
+                    end
+
+                    else begin
+                        // send signals to stall instruction 3
+                        // this one is necessary, because it could be that all 3 are ALU ops, or just one of the others is 
+                    end
+
+                end
+            end
+
+            // else begin
+            //     // send signals to stall instruction 2 and 3
+            //     // this one is necessary, because it could be that inst's 1 and 2 are both memory ops, in which case, not enough piplines for both
+                
+            // end
+
+        end
+        else begin
+            // output stall signals for:
+            // all 3 PCs
+            // all 3 inst outputs
+            inst1_pc_out = first_pc
+            inst2_pc_out = second_pc;
+            inst3_pc_out = third_pc;
+
+            // Pipeline A -- ALU 1
+            pipeA_op_in = 6'b000000;
+            pipeA_dest_in = 5'b00000;
+            pipeA_src1_in = 5'b00000;
+            pipeA_src2_in = 5'b00000;
+            pipeA_func_in = 6'b000000;
+            pipeA_shamt_in = 5'b00000;
+            pipeA_imm_in = 16'h0000;
+
+            // Pipeline B -- ALU 2
+            pipeB_op_in = 6'b000000;
+            pipeB_dest_in = 5'b00000;
+            pipeB_src1_in = 5'b00000;
+            pipeB_src2_in = 5'b00000;
+            pipeB_func_in = 6'b000000;
+            pipeB_shamt_in = 5'b00000;
+            pipeB_imm_in = 6'h0000;
+
+            // Pipeline C -- Memory Ops
+            pipeC_op_in = 6'b000000;
+            pipeC_dest_in = 5'b00000;
+            pipeC_src1_in = 5'b00000;
+            pipeC_imm_in = 16'h0000;
+
+        end
         // && that should be all she wrote
         
 
