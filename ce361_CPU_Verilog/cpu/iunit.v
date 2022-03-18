b28c6de8f108d0ec640f5972d79f9768427deab2
jacobdzierwa <39633156+jacobdzierwa@users.noreply.github.com>
2022-03-17 23:27:59 -0500

    Update iunit.v

--- ce361_CPU_Verilog/cpu/iunit.v
+++ ce361_CPU_Verilog/cpu/iunit.v
@@ -17,6 +17,9 @@ module iunit (clk, curr_pc0, curr_pc1, curr_pc2, instr0, instr1, instr2, pc_plus
     input clk;
 
 
+    
+
+
 
     // program counter (already in CPU)
    //  reg_32 prog_counter (.clk(clk), .d(next_pc), .q(curr_pc));
@@ -28,7 +31,29 @@ module iunit (clk, curr_pc0, curr_pc1, curr_pc2, instr0, instr1, instr2, pc_plus
     defparam instr_mem.mem_file = dat_file;
 
     // adds "1" to PC
-    adder_32 pc_adder(.a(curr_pc), .b(32'b00000000000000000000000000000100), .z(pc_plus_4));
+    //adder_32 pc_adder(.a(curr_pc), .b(32'b00000000000000000000000000000100), .z(pc_plus_4));
+
+    always@ (curr_pc0 || curr_pc1 || curr_pc2) begin
+        
+        if (!curr_pc2)begin // if instruction 3 stalled
+            adder_32 pc_adder0(.a(curr_pc0), .b(32'b00000000000000000000000000000100), .z(pc_plus_4));
+            adder_32 pc_adder1(.a(curr_pc0), .b(32'b00000000000000000000000000000100), .z(pc_plus_4));
+        end
+
+        else if (!curr_pc2 && !curr_pc1)begin // if 2 + 3 stalled
+            adder_32 pc_adder2(.a(curr_pc0), .b(32'b00000000000000000000000000000100), .z(pc_plus_4));
+        end
+
+        else if (!curr_pc2 && !curr_pc1 && !curr_pc0)begin // if all stalled
+            //adder_32 pc_adder2(.a(curr_pc2), .b(32'b00000000000000000000000000000100), .z(pc_plus_4));
+        end
+
+        else begin // none stalled
+            adder_32 pc_adder3(.a(curr_pc0), .b(32'b00000000000000000000000000000100), .z(pc_plus_4));
+            adder_32 pc_adder4(.a(curr_pc1), .b(32'b00000000000000000000000000000100), .z(pc_plus_4));
+            adder_32 pc_adder5(.a(curr_pc2), .b(32'b00000000000000000000000000000100), .z(pc_plus_4));
+        end
+    end
 
     // mux to determine next instruction (0: PC +=4, 1: PC = jump to next instr) already in CPU
     //mux_n next_instr_mux(.sel(branch_ctrl), .src0(pc4_wire), .src1(target), .z(next_pc));
